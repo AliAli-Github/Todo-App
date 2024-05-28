7a89432c01fff564429421cb319548dac40ccede
AliAli-Github <alimurtaza3721@gmail.com>
2024-05-28 11:15:49 +0500

    edit task function remaining.

--- script.js
+++ script.js
@@ -1,39 +1,51 @@
-const textarea = document.getElementById("prompt-textarea");
+const textarea = document.querySelector("#prompt-textarea");
 const addTodoBtn = document.querySelector("#add-task-btn");
 const taskList = document.querySelector(".tasks-list");
-let allTodos = [];
-textarea.addEventListener('input', function () {
-    if (textarea.value !== "") {
-        addTodoBtn.style.backgroundColor = "black";
-        addTodoBtn.removeAttribute("disabled");
-    }
-    else {
-        addTodoBtn.style.backgroundColor = "#d7d7d7";
-        addTodoBtn.setAttribute("disabled", "disabled");
-    }
-    this.style.height = "auto";
-    this.style.height = `${this.scrollHeight}px`;
-});
+
+function checkPrompt() {
+    textarea.addEventListener('input', function () {
+        if (textarea.value !== "") {
+            addTodoBtn.style.backgroundColor = "black";
+            addTodoBtn.removeAttribute("disabled");
+        }
+        else {
+            addTodoBtn.style.backgroundColor = "#d7d7d7";
+            addTodoBtn.setAttribute("disabled", "disabled");
+        }
+        this.style.height = "auto";
+        this.style.height = `${this.scrollHeight}px`;
+    });
+}
+
+checkPrompt();
 
 function addTodo() {
+
     addTodoBtn.addEventListener("click", () => {
-        taskList.innerHTML += ` <div class="task">
-        <span class="normal"></span>
-        <p>${textarea.value}</p>
-        <i onmouseenter="showMenu(this)" class="ri-more-2-line options-btn"></i>
-        <div onmouseleave="hideMenu(this)" class="menu">
-            <li onclick="deleteTask(this)"><i class="ri-delete-bin-line"></i> Delete</li>
-            <li><i class="ri-pencil-line"></i> Edit</li>
-            <li onclick="changeColor(this,'#ff00003e','#ff0000')"><span class="red"></span> Urgent</li>
-            <li onclick="changeColor(this,'#ffff0038','#daa520')"><span class="yellow"></span> Important</li>
-            <li onclick="changeColor(this,'#87cfeb51','#0f99d0')"><span class="blue"></span> Assigned</li>
-        </div>
-    </div>`;
-        textarea.value = "";
-    })
+        if (textarea.value !== "") {
+            taskList.innerHTML += ` <div class="task">
+            <span class="normal"></span>
+            <p>${textarea.value}</p>
+            <i onmouseenter="showMenu(this)" class="ri-more-2-line options-btn"></i>
+            <div onmouseleave="hideMenu(this)" class="menu">
+                <li onclick="deleteTask(this)"><i class="ri-delete-bin-line"></i> Delete</li>
+                <li onclick="editTodo()"><i class="ri-pencil-line"></i> Edit</li>
+                <li onclick="changeColor(this,'#ff00003e','#ff0000')"><span class="red"></span> Urgent</li>
+                <li onclick="changeColor(this,'#ffff0038','#daa520')"><span class="yellow"></span> Important</li>
+                <li onclick="changeColor(this,'#87cfeb51','#0f99d0')"><span class="blue"></span> Assigned</li>
+            </div>
+        </div>`;
+            textarea.value = "";
+        }
+    });
+
 }
 
 addTodo();
+
+function editTodo() {
+    
+}
 
 function changeColor(elem, bgColor, border) {
     let targetElem = elem.parentNode.parentNode.childNodes;
